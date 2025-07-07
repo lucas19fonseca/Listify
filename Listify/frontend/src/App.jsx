33b67f9a645003efaf2bea93c77a2bae601d9b08
lucas19fonseca <lucas19fonseca@gmail.com>
2025-07-07 19:39:22 -0300

    da

---
+++ Listify/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      dada
+    </div>
+  )
+}
+
+export default App
